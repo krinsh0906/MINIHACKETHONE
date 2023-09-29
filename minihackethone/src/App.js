ce27f44eb8f25248518c63993f899e65f0236ea6
krinsh panchal <jayveer1301@gmail.com>
2023-09-30 01:32:05 +0530

    add this login page

---
+++ minihackethone/src/App.js
@@ -0,0 +1,33 @@
+import Login from './login'
+
+import './App.css';
+import './login.css';
+// import Homenav from './homenav';
+// import './homenav.css';
+// import Hero from './hero';
+// import './hero.css'
+// import Hometodo from './hometodo';
+// import './hometodo.css'
+// import Contactus from './contactus';
+// import './contactus.css'
+// import Footer from './footer';
+// import './footer.css'
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <Login/>
+      {/* <Homenav/>
+      <Hero/>
+      <Hometodo/>
+      <Contactus/>
+      <Footer/> */}
+
+    </div>
+  );
+}
+
+export default App;
