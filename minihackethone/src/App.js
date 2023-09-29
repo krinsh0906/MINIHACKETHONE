4fd6aae9db54cbcb80ed90856ea7ffbcb4fab016
krinsh panchal <jayveer1301@gmail.com>
2023-09-30 03:22:51 +0530

    add this todo

--- minihackethone/src/App.js
+++ minihackethone/src/App.js
@@ -6,8 +6,8 @@ import Homenav from './homenav';
 import './homenav.css';
 import Hero from './hero';
 import './hero.css'
-// import Hometodo from './hometodo';
-// import './hometodo.css'
+import Hometodo from './hometodo';
+import './hometodo.css'
 // import Contactus from './contactus';
 // import './contactus.css'
 // import Footer from './footer';
@@ -22,8 +22,8 @@ function App() {
       {/* <Login/> */}
        <Homenav/>
       <Hero/>
-      {/* <Hometodo/>
-      <Contactus/>
+      <Hometodo/>
+      {/* <Contactus/>
       <Footer/>  */}
 
     </div>
---
+++ minihackethone/src/hometodo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import todo1 from './assest/todo2.jpg'
+function Hometodo() {
+    return (
+        <>
+            <hr className='todo-line'></hr>
+            <div className='maintodo-container'>
+
+                <div className='todo-logo'>
+                    <h2>Todo List</h2>
+
+                </div>
+                <div className='todo-text'>
+                    <p>Todolist is a simple and effective tool used for task management and organization.<br /> To-do lists are commonly used in both personal and professional settings to help<br /> people stay organized, manage their time efficiently, and ensure important tasks<br /> are not forgotten.</p>
+                    <div className='todo-button'>
+                        <button className='button' type='submit'>Get Start</button>
+                    </div>
+                </div>
+
+                <div className='todo-img' >
+                    <img className='todo-vactor' src={todo1}></img>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Hometodo;
