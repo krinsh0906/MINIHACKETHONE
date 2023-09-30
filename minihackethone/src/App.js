e7e41c2c19a61c785b87992de9158737f6a5ca62
krinsh panchal <jayveer1301@gmail.com>
2023-10-01 04:55:43 +0530

    add this add task

--- minihackethone/src/App.js
+++ minihackethone/src/App.js
@@ -1,17 +1,18 @@
 // import Login from './login'
+// import './login.css'
 
 
 // import './App.css';
-import Completed from './completed';
-import './completed.css'
-import Inprogress from './inprogress';
-import './inprogress.css'
-import Todoadd from './todoadd';
-import './todoadd.css'
-import Todonav from './todonav';
-import './todonav.css'
-import Todopage from './todopage';
-import './todopage.css'
+// import Completed from './completed';
+// import './completed.css'
+// import Inprogress from './inprogress';
+// import './inprogress.css'
+// import Todoadd from './todoadd';
+// import './todoadd.css'
+// import Todonav from './todonav';
+// import './todonav.css'
+// import Todopage from './todopage';
+// import './todopage.css'
 // import './login.css';
 // import Homenav from './homenav';
 // import './homenav.css';
@@ -25,17 +26,9 @@ import './todopage.css'
 // import './footer.css'
 
 import './App.css';
-// import './login.css';
-import Homenav from './homenav';
-import './homenav.css';
-import Hero from './hero';
-import './hero.css'
-import Hometodo from './hometodo';
-import './hometodo.css'
-import Contactus from './contactus';
-import './contactus.css'
-import Footer from './footer';
-import './footer.css'
+import Addtask from './addtask';
+import './addtask.css'
+
 
 
 
@@ -51,17 +44,14 @@ function App() {
       <Hometodo/>
       <Contactus/>
       <Footer/> */}
-      <Todonav/>
+      {/* <Todonav/>
       <Todopage/>
       <Todoadd/>
       <Inprogress/>
-      <Completed/>
+      <Completed/> */}
+      <Addtask/>
 
-       <Homenav/>
-      <Hero/>
-      <Hometodo/>
-      <Contactus/>
-      <Footer/> 
+      
 
 
     </div>
